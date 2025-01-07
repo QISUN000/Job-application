82bc88be8c4f5bed783880e751a353c14e4e0bb5
QISUN000 <qsun4@rrc.ca>
2025-01-07 11:35:26 -0600

    Update JobService.java

--- src/main/java/com/example/jobapplication/Job/JobService.java
+++ src/main/java/com/example/jobapplication/Job/JobService.java
@@ -5,6 +5,7 @@ import java.util.List;
 public interface JobService {
     List<Job> findAll();
     void createJob(Job job);
+//    update
 
     Job findById(long id);
     Job deleteById(long id);
